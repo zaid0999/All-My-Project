a428a6d121b9fe71c616df0a3778cebcf494fbf6
Zaid <zaid0999@gmail.com>
2025-03-17 14:14:34 +0530

    javascript updated

--- Javascript/datatypes/script.js
+++ Javascript/datatypes/script.js
@@ -9,3 +9,31 @@ console.log(a, typeof a)
 a=true;
 a=1+true;
 console.log(a,typeof a)
+
+
+
+let p = 25 - 'p';
+p = Infinity;
+p=5.4183
+console.log(p,typeof p)
+console.log(p.toFixed(2))
+p=25;
+console.log(p.toString(2))
+p=5 + + '5';
+console.log(p)
+
+// Math methods
+
+console.log(Math.sqrt(16))
+console.log(Math.cbrt(27))
+console.log(Math.abs(-10))
+console.log(Math.max(5,6,3,5,8,9,6))
+console.log(Math.min(2,9,3,5,6,2,1))
+console.log(Math.ceil(4.2))
+console.log(Math.floor(4.9))
+console.log(Math.round(5.9))
+console.log(Math.round(5.4))
+console.log(Math.round(Math.random() *10000))
+console.log(Math.pow(2,3), 2**4)
+
+
